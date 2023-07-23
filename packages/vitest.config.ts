52e4338e0daf945e60ac6d363902902be1af398d
金鱼 <2115756856@qq.com>
2023-07-23 21:49:09 +0800

    Update vitest.config.ts

--- packages/vitest.config.ts
+++ packages/vitest.config.ts
@@ -1,7 +1,11 @@
-import { defineConfig } from 'vitest/config'
+import { defineConfig } from "vitest/config";
+import path from "path";
+
+const resolveFile = (file: string) => path.resolve(__dirname, file);
 
 export default defineConfig({
   test: {
-    // ...
+    // 运行在每个测试文件前面
+    setupFiles: [resolveFile("./test/setupFiles/index.ts")],
   },
-})
+});
