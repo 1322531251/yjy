d3e8d675510bfc1dea909d8adfaf722dd61dea0c
金鱼 <2115756856@qq.com>
2023-10-09 21:24:57 +0800

    Create App.vue

---
+++ vue_test/day12mixin/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+      <School></School>
+      <hr>
+      <Student/>
+    </div>
+</template>
+
+<script>
+  import School from './components/School.vue';
+  import Student from './components/Student.vue';
+  export default {
+    name:'App',
+    methods:{
+
+    },
+    components:{School,Student},
+
+  }
+</script>
+
+<style>
+
+</style>
