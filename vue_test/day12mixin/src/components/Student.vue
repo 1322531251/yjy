c0c988b96a397bf5b2f7f2893d8c2894eda2272a
金鱼 <2115756856@qq.com>
2023-10-09 21:25:08 +0800

    Create Student.vue

---
+++ vue_test/day12mixin/src/components/Student.vue
@@ -0,0 +1,28 @@
+<!-- 单文件组件 -->
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <h2 @click="showName">学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+    </div>
+</template>
+
+<script>
+    //引入一个混合
+    // import {mixin,mixin2} from '../mixin'
+    //组件交互相关的代码(数据,方法等等)
+    export default{
+        name:'Student',
+        data(){
+            return{
+                name:'lili',
+                sex:'女',
+                x:66
+
+            }
+        },
+        // mixins:[mixin,mixin2]
+
+    }
+    
+</script>
