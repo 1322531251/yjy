0481ef164b1065fc68c8dd4594d5787132caa5df
金鱼 <2115756856@qq.com>
2023-10-09 21:26:05 +0800

    Create App.vue

---
+++ vue_test/day12props/src/App.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+      <h1 v-text="msg" ref="title"></h1>
+      <button ref="btn" @click="showDOM">点我输出dom元素</button>
+      <School ref="sch"></School>
+      <Student name="lili" />
+    </div>
+</template>
+
+<script>
+  import School from './components/School.vue';
+  import Student from './components/Student.vue';
+  export default {
+    name:'App',
+    data(){
+      return{msg:'欢迎学习vue'}
+    },
+    methods:{
+      showDOM(){
+        console.log(this.$refs.title);//真实DOM元素
+        console.log(this.$refs.btn);//真实DOM元素
+        console.log(this.$refs.sch); //School组件的实例对象（vc）
+      }
+    },
+    components:{School,Student},
+
+  }
+</script>
+
+<style>
+
+</style>
