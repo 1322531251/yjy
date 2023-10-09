68b3366efad0a5d3bbbacffde5828a298fa575c7
金鱼 <2115756856@qq.com>
2023-10-09 21:25:38 +0800

    Create School.vue

---
+++ vue_test/day12plugins/src/components/School.vue
@@ -0,0 +1,36 @@
+<!-- 单文件组件 -->
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2 >学校名称：{{name | mySlice}}</h2>
+        <h2>学校地址：{{address}}</h2>
+        <button @click="test">点我弹窗</button>
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码(数据,方法等等)
+    export default{
+        name:'School',
+        data(){ 
+            return{
+                name:'黑科技123456',
+                address:'黑龙江',
+            }
+        },
+        methods: {
+            test(){
+                this.hello()
+            }
+        },
+
+    }
+    
+</script>
+
+<style>
+    /* 组件的样式 */
+    .demo{
+        background-color: pink;
+    }
+</style>
