e3f4ad39551822f0989ec4876c53385327ec5625
金鱼 <2115756856@qq.com>
2023-10-09 21:26:54 +0800

    Create School.vue

---
+++ vue_test/day12scoped/src/components/School.vue
@@ -0,0 +1,29 @@
+<!-- 单文件组件 -->
+<template>
+    <!-- 组件的结构 -->
+    <div class="demo">
+        <h2 >学校名称：{{name}}</h2>
+        <h2>学校地址：{{address}}</h2>
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码(数据,方法等等)
+    export default{
+        name:'School',
+        data(){ 
+            return{
+                name:'黑科技',
+                address:'黑龙江',
+            }
+        },
+    }
+    
+</script>
+
+<style scoped>
+    /* 组件的样式 */
+    .demo{
+        background-color: pink;
+    }
+</style>
