a082d9adfb70beaea8d6409ac4710d1cabf36ee0
金鱼 <2115756856@qq.com>
2023-10-09 21:27:16 +0800

    Update App.vue

--- vue_test/src/App.vue
+++ vue_test/src/App.vue
@@ -1,30 +1,22 @@
 <template>
     <div>
-      <h1 v-text="msg" ref="title"></h1>
-      <button ref="btn" @click="showDOM">点我输出dom元素</button>
-      <School ref="sch"></School>
+      <School></School>
+      <hr>
+      <Student/>
     </div>
 </template>
 
 <script>
   import School from './components/School.vue';
+  import Student from './components/Student.vue';
+  
   export default {
     name:'App',
-    data(){
-      return{msg:'欢迎学习vue'}
-    },
-    methods:{
-      showDOM(){
-        console.log(this.$refs.title);//真实DOM元素
-        console.log(this.$refs.btn);//真实DOM元素
-        console.log(this.$refs.sch); //School组件的实例对象（vc）
-      }
-    },
-    components:{School},
+    components:{School,Student},
 
   }
 </script>
 
 <style>
-
+  
 </style>
