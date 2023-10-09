97aa0d230d28bc903b9fdeecc4d9d2b9abf75d0e
金鱼 <2115756856@qq.com>
2023-10-09 21:25:41 +0800

    Create Student.vue

---
+++ vue_test/day12plugins/src/components/Student.vue
@@ -0,0 +1,23 @@
+<!-- 单文件组件 -->
+<template>
+    <!-- 组件的结构 -->
+    <div>
+        <h2>学生姓名：{{name}}</h2>
+        <h2>学生性别：{{sex}}</h2>
+        <input type="text" v-fbind:value="name">
+    </div>
+</template>
+
+<script>
+    //组件交互相关的代码(数据,方法等等)
+    export default{
+        name:'Student',
+        data(){
+            return{
+                name:'lili',
+                sex:'女',
+            }
+        },
+    }
+    
+</script>
