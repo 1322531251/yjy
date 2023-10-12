0bb5149160ac3593c21dcd126f7a65bf25680d71
金鱼 <2115756856@qq.com>
2023-10-13 00:06:19 +0800

    Create main.js

---
+++ vue_test/day15 GlobalEventBus/src/main.js
@@ -0,0 +1,16 @@
+// 引入Vue
+import Vue from 'vue'
+// 引入App
+import App from './App.vue'
+
+// 关闭生产提示
+Vue.config.productionTip = false
+
+new Vue({
+    el:'#app',
+    render: h => h(App),
+    beforeCreate() {
+        Vue.prototype.$bus = this  //安装全局事件总线
+    },
+})
+
