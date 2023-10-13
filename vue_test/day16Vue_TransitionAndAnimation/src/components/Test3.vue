188a411ac9f35424a2e76566e4db39b2b02f0e04
金鱼 <2115756856@qq.com>
2023-10-13 21:55:15 +0800

    Create Test3.vue

---
+++ vue_test/day16Vue_TransitionAndAnimation/src/components/Test3.vue
@@ -0,0 +1,33 @@
+<template>
+    <div>
+      <button @click="isShow = !isShow">显示/隐藏</button>
+      <transition-group 
+      name="animate__animated animate__bounce" 
+      appear
+      enter-active-class="animate__swing"
+      leave-active-class="animate__backOutUp"
+      >
+          <h1 v-show="!isShow" key="1">你好</h1>
+          <h1 v-show="isShow" key="2">hello</h1>
+      </transition-group>
+  </div>
+  </template>
+  
+  <script>
+    import 'animate.css'
+    export default {
+        name:'Test3',
+        data() {
+            return {
+                isShow:true 
+            }
+        },
+    }
+  </script>
+  
+  <style>
+      h1{
+          background-color: orange;
+      }
+
+  </style>
