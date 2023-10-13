04971e5fab3f4c409534b3745a88fbbefb49b5dc
金鱼 <2115756856@qq.com>
2023-10-13 21:55:05 +0800

    Create Test.vue

---
+++ vue_test/day16Vue_TransitionAndAnimation/src/components/Test.vue
@@ -0,0 +1,42 @@
+<template>
+  <div>
+    <button @click="isShow = !isShow">显示/隐藏</button>
+    <transition name="move" appear>
+        <h1 v-show="isShow">你好</h1>
+    </transition>
+</div>
+</template>
+
+<script>
+export default {
+    name:'Test',
+    data() {
+        return {
+            isShow:true
+        }
+    },
+}
+</script>
+
+<style>
+    h1{
+        background-color: orange;
+    }
+
+    .move-enter-active{
+        animation: move 1s;
+    }
+
+    .move-leave-active{
+        animation: move 1s reverse;
+    }
+
+    @keyframes move {
+         from{
+            transform: translateX(-100%);
+         }to{
+            transform: translateX(0px);
+
+         }
+    }
+</style>
