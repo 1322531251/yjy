24f952edac9b8cb96fc8f9a16898fd8599df1858
金鱼 <2115756856@qq.com>
2023-10-19 00:08:14 +0800

    Create App.vue

---
+++ vue_test/day20multiComponentSharedData/src/App.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <Count/>
+    <hr>
+    <Person/>
+  </div>
+</template>
+
+<script>
+  import Person from './components/Person'
+  import Count from './components/Count'
+
+  export default {
+      name:'App',
+      components:{Count,Person},
+  }
+</script>
