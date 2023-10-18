6114a9ef67b104b052f4577e2f89c59843d149db
金鱼 <2115756856@qq.com>
2023-10-18 13:39:43 +0800

    Create Count.vue

---
+++ vue_test/day19store/src/components/Count.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+        <h1>当前求和为：{{sum}}</h1>
+        <select v-model="n">
+            <option :value="1">1</option>
+            <option :value="2">2</option>
+            <option :value="3">3</option>
+        </select>
+        <button @click="increment">+</button>
+        <button @click="decrement">-</button>
+        <button @click="incrementOdd">当前求和为奇数时+</button>
+        <button @click="incrementWait">等一会再加</button>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Count',
+        data() {
+            return {
+                sum:0, //当前和
+                n:1, //用户选择的数字     
+            }
+        },
+        methods: {
+            increment(){
+                this.sum += this.n
+            },
+            decrement(){
+                this.sum -= this.n
+            },
+            incrementOdd(){
+                if(this.sum % 2){
+                    this.sum += this.n
+                }
+            },
+            incrementWait(){
+                setTimeout(()=>{
+                    this.sum += this.n
+                },500)
+            }
+        },
+    }
+</script>
+
+<style>
+    button{
+        margin-left: 5px;
+    }
+</style>
