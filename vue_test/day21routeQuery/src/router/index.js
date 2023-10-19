7b6da2f1e8a493e8d762665c6de7e93b06ad2598
金鱼 <2115756856@qq.com>
2023-10-19 22:22:20 +0800

    Create index.js

---
+++ vue_test/day21routeQuery/src/router/index.js
@@ -0,0 +1,37 @@
+//该文件用于创建整个应用的路由器
+import VueRouter from 'vue-router'
+//引入组件
+import About from '../pages/About'
+import Home from '../pages/Home'
+import News from '../pages/News'
+import Message from '../pages/Message'
+import Detail from '../pages/Detail'
+
+export default new VueRouter({
+    routes:[
+        {
+            path:'/about',
+            component:About,
+        },        
+        {
+            path:'/home',
+            component:Home,
+            children:[
+                {
+                    path:'news',
+                    component:News,
+                },                
+                {
+                    path:'message',
+                    component:Message,
+                    children:[
+                        {
+                            path:'detail',
+                            component:Detail,
+                        },           
+                    ]
+                },
+            ]
+        },
+    ],
+})
