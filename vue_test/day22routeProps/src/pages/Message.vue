a551277522a5592350762e23c6f5ccea81416729
金鱼 <2115756856@qq.com>
2023-10-20 23:01:05 +0800

    Create Message.vue

---
+++ vue_test/day22routeProps/src/pages/Message.vue
@@ -0,0 +1,42 @@
+<template>
+    <div>
+        <ul>
+        <li v-for="msg in messageList" :key="msg.id">
+            <!-- 跳转路由并携带params参数 to的字符串写法 -->
+            <!-- <router-link :to="`/home/message/detail/${msg.id}/${msg.title}`">{{msg.title}}</router-link>&nbsp;&nbsp; -->
+            
+            <!-- 跳转路由并携带params参数 to的对象写法 -->
+            <router-link :to="{
+                name:'xiangqing',
+                query:{
+                    id:msg.id,
+                    title:msg.title
+                }
+            }">
+                {{msg.title}}
+            </router-link>
+        </li>
+        </ul>
+        <hr>
+        <router-view></router-view>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Message',
+        data() {
+            return {
+                messageList:[
+                    {id:'001',title:'消息001'},
+                    {id:'002',title:'消息002'},
+                    {id:'003',title:'消息003'},
+                ]
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
