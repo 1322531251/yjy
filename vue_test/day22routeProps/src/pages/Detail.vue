3fc33aecc1cfe67eee633710a1f7a99cb666e06c
金鱼 <2115756856@qq.com>
2023-10-20 23:01:02 +0800

    Create Detail.vue

---
+++ vue_test/day22routeProps/src/pages/Detail.vue
@@ -0,0 +1,21 @@
+<template>
+    <ul>
+        <li>消息编号:{{id}}</li>
+        <li>消息标题:{{title}}</li>
+        <!-- <li>a:{{a}}</li>
+        <li>b:{{b}}</li> -->
+    </ul>
+</template>
+
+<script>
+    export default {
+        name:'Detail',
+        // props:['a','b']
+        props:['id','title']
+
+    }
+</script>
+
+<style>
+
+</style>
