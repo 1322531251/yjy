47f5729d5f9d044b164f5aa43acb724ae2b4dcc8
金鱼 <2115756856@qq.com>
2023-10-20 22:57:21 +0800

    Create Banner.vue

---
+++ vue_test/day22activatedAndDeactivated/src/components/Banner.vue
@@ -0,0 +1,31 @@
+<template>
+    <div class="col-xs-offset-2 col-xs-8">
+        <div class="page-header">
+            <h2>Vue Router Demo</h2>
+            <button @click="back">后退</button>
+            <button @click="forward">前进</button>
+            <button @click="go">go</button>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        name:'Banner',
+        methods: {
+            back(){
+                this.$router.back()
+            },
+            forward(){
+                this.$router.forward()
+            },
+            go(){
+                this.$router.go(-2) //后退2步 如果是正数就是前进2步
+            }
+        },
+    }
+</script>
+
+<style>
+
+</style>
