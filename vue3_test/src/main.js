39a993f1c04b480f4b479d43a1fa651630076918
金鱼 <2115756856@qq.com>
2023-10-23 22:27:04 +0800

    Update main.js

--- vue3_test/src/main.js
+++ vue3_test/src/main.js
@@ -1,4 +1,11 @@
+//引入的不再是Vue构造函数，引入的是叫createApp的工厂函数
 import { createApp } from 'vue'
 import App from './App.vue'
 
-createApp(App).mount('#app')
+
+//创建应用实例对象-app(类似于Vue2中的vm，但app比vm更轻)
+// createApp(App).mount('#app')
+const app = createApp(App)
+
+//挂载
+app.mount('#app')
