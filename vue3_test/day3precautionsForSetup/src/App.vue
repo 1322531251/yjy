389dfff698795fb59d6b3257a22800cfff798262
金鱼 <2115756856@qq.com>
2023-10-25 21:44:00 +0800

    Create App.vue

---
+++ vue3_test/day3precautionsForSetup/src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <Demo @hello='showHelloMsg' msg="你好" school="黑科技">
+    <template v-slot:hi>
+      <span>hi</span>
+    </template>
+  </Demo>
+</template>
+
+<script>
+  import Demo from './components/Demo'
+
+  export default {
+    name: 'App',
+    components:{Demo},
+    setup() {
+      function showHelloMsg(value){
+        alert(`参数${value}`)
+      }
+      return {
+        showHelloMsg
+      }
+    }
+  }
+
+</script>
+
