c922da557f61738a643e7f0e38b1a8f1da62bef0
金鱼 <2115756856@qq.com>
2023-10-25 21:43:36 +0800

    Create Demo.vue

---
+++ vue3_test/day3computed/src/components/Demo.vue
@@ -0,0 +1,50 @@
+<template>
+    <div>
+    <h1>信息</h1>
+    姓：<input type="text" v-model="person.firstName"><br><br>
+    名：<input type="text" v-model="person.lastName"><br><br>
+    <span>全名：{{person.fullName}}</span><br><br>
+    全名：<input type="text" v-model="person.fullName">
+    </div>
+  </template>
+  
+  <script>
+    import {reactive,computed} from 'vue'
+  
+    export default {
+      name: 'Demo',
+
+      setup(){
+        let person = reactive({
+          firstName:'张',
+          lastName:'三',
+        })
+
+
+        //计算属性
+        // person.fullName = computed(()=>{
+        //   return person.firstName + '-' + person.lastName
+        // })
+
+        person.fullName = computed({
+          get(){
+            return person.firstName + '-' + person.lastName
+          },
+          set(value){
+            const nameArr = value.split('-')
+            person.firstName = nameArr[0]
+            person.lastName = nameArr[1]
+          }
+        })
+
+        return{
+            person,
+        }
+      },
+
+
+    }
+  
+  </script>
+  
+  
