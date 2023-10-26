a90e8a36f7c0d95b5400bdbba45d017e11fe45dd
金鱼 <2115756856@qq.com>
2023-10-26 22:47:57 +0800

    Create Demo.vue

---
+++ vue3_test/day4watch_value/src/components/Demo.vue
@@ -0,0 +1,64 @@
+<template>
+  <div>
+    <h2>当前求和为：{{sum}}</h2>
+    <button @click="sum++">sum++</button>
+    <hr>
+    <h2>{{msg}}</h2>
+    <button @click="msg += '!'">修改</button>
+    <hr>
+    <h2>姓名：{{person.name}}</h2>
+    <h2>年龄：{{person.age}}</h2>
+    <h2>薪资：{{person.job.j1.salary}}k</h2>
+    <button @click="person.name += '~'">修改姓名</button>
+    <button @click="person.age++">修改年龄</button>
+    <button @click="person.job.j1.salary++">涨薪</button>
+  </div>
+</template>
+  
+<script>
+    import {ref,watch,reactive} from 'vue'
+  
+    export default {
+      name: 'Demo',
+
+      setup(){
+        let sum = ref(0)
+        let msg = ref('你好')
+
+        let person = ref({
+          name:'张三',
+          age:18,
+          job:{
+            j1:{
+              salary:20
+            }
+          }
+        })
+
+        //基本类型不能.value
+        watch(sum,(newValue,oldValue)=>{
+          console.log('sum变了',newValue,oldValue);
+        })
+
+        // watch(person.value,(newValue,oldValue)=>{
+        //   console.log('person变了',newValue,oldValue);
+        // })
+
+        watch(person,(newValue,oldValue)=>{
+          console.log('person变了',newValue,oldValue);
+        },{deep:true})
+
+
+        return{
+            sum,
+            msg,
+            person
+        }
+      },
+
+
+    }
+  
+</script>
+  
+  
