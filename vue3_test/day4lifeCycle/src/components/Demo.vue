3c052512e21526df3ec052113a5573129b845247
金鱼 <2115756856@qq.com>
2023-10-26 22:47:12 +0800

    Create Demo.vue

---
+++ vue3_test/day4lifeCycle/src/components/Demo.vue
@@ -0,0 +1,47 @@
+<template>
+  <div>
+    <h2>当前求和为：{{sum}}</h2>
+    <button @click="sum++">sum++</button>
+  </div>
+</template>
+  
+<script>
+    import {ref,onBeforeMount,onMounted,onBeforeUpdate,onUpdated,onBeforeUnmount,onUnmounted} from 'vue'
+  
+    export default {
+      name: 'Demo',
+
+      setup(){
+        let sum = ref(0)
+
+        onBeforeMount(()=>{
+          console.log('----onBeforeMount----');
+        })        
+        onMounted(()=>{
+          console.log('----onMounted----');
+        }) 
+        onBeforeUpdate(()=>{
+          console.log('----onBeforeUpdate----');
+        })
+        onUpdated(()=>{
+          console.log('----onUpdated----');
+        })        
+        onBeforeUnmount(()=>{
+          console.log('----onBeforeUnmount----');
+        })       
+        onUnmounted(()=>{
+          console.log('----onUnmounted----');
+        })        
+
+
+
+        return{
+            sum,
+        }
+      },
+      
+    }
+  
+</script>
+  
+  
