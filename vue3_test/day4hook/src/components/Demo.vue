f9137746d411907bd808c90d4a3f56cf78a573c0
金鱼 <2115756856@qq.com>
2023-10-26 22:46:47 +0800

    Create Demo.vue

---
+++ vue3_test/day4hook/src/components/Demo.vue
@@ -0,0 +1,31 @@
+<template>
+  <div>
+    <h2>当前求和为：{{sum}}</h2>
+    <button @click="sum++">sum++</button>
+    <hr>
+    <h2>当前点击的鼠标坐标为：x:{{point.x}}，y:{{point.y}}</h2>
+  </div>
+</template>
+  
+<script>
+    import {ref} from 'vue'
+    import usePoint from '../hooks/usePoint'
+
+    export default {
+      name: 'Demo',
+
+      setup(){
+        let sum = ref(0)
+        let point = usePoint()
+
+        return{
+            sum,
+            point
+        }
+      },
+      
+    }
+  
+</script>
+  
+  
