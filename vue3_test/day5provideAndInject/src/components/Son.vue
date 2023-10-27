8804b4f1040a0020e35d25fa0c4b4afac9fdcabe
金鱼 <2115756856@qq.com>
2023-10-27 23:24:59 +0800

    Create Son.vue

---
+++ vue3_test/day5provideAndInject/src/components/Son.vue
@@ -0,0 +1,23 @@
+<template>
+    <div class="son">
+      <h3>孙组件,{{car.name}}---{{car.price}}</h3>
+    </div>
+  </template>
+  
+  <script>
+    import {inject} from 'vue'
+    export default {
+      name:'Son',
+      setup() {
+        let car = inject('car')
+        return {car}
+      }
+    }
+  </script>
+  
+  <style>
+    .son{
+      background-color: green;
+      padding: 10px;
+    }
+  </style>
