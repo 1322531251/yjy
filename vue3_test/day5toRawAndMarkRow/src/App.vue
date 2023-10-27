8e255a4a21b43902e681f783d75dc60aaf3efc5b
金鱼 <2115756856@qq.com>
2023-10-27 23:26:40 +0800

    Create App.vue

---
+++ vue3_test/day5toRawAndMarkRow/src/App.vue
@@ -0,0 +1,25 @@
+<template>
+  <div>
+    <button @click="isShowDemo = !isShowDemo">切换隐藏/显示</button>
+    <Demo v-if="isShowDemo"/>
+  </div>
+</template>
+
+<script>
+  import {ref} from 'vue'
+  import Demo from './components/Demo'
+
+  export default {
+    name: 'App',
+    components:{Demo},
+    setup() {
+      let isShowDemo = ref(true)
+
+      return{
+        isShowDemo
+      }
+    }
+  }
+
+</script>
+
