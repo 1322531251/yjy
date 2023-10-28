e36d6981a4eb4012e3e7d29fd050ad02767e25f9
金鱼 <2115756856@qq.com>
2023-10-28 19:13:50 +0800

    Create Child.vue

---
+++ vue3_test/src/components/Child.vue
@@ -0,0 +1,28 @@
+<template>
+    <div class="child">
+      <h3>child</h3>
+      {{sum}}
+    </div>
+</template>
+  
+<script>
+    import {ref} from 'vue'
+    export default {
+      name:'Child',
+      setup() {
+        let sum = ref(0)
+        return new Promise((resolve,reject)=>{
+          setTimeout(()=>{
+          resolve(sum)
+          },200)
+        })
+      }
+    }
+</script>
+  
+<style>
+    .child{
+      background-color: skyblue;
+      padding: 10px;
+    }
+</style>
