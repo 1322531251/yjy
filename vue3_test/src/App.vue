903a65436c1868be84e357fabc472ff8ed9ffd93
金鱼 <2115756856@qq.com>
2023-10-28 19:13:46 +0800

    Update App.vue

--- vue3_test/src/App.vue
+++ vue3_test/src/App.vue
@@ -1,70 +1,34 @@
 <template>
-  <div>
-  <h1>信息</h1>
-  <h2 v-show="person.name">姓名：{{person.name}}</h2>
-  <h2>年龄：{{person.age}}</h2>
-  <h2 v-show="person.sex">性别：{{person.sex}}</h2>
-  <h3>工作种类：{{person.job.type}}</h3>
-  <h3>工作薪水：{{person.job.salary}}</h3>
-  <h3>爱好：{{person.hobby}}</h3>
-  <h3>test:{{person.job.a.b.c}}</h3>
-  <button @click="changeInfo">修改信息</button>
-  <button @click="addSex">添加sex属性</button>
-  <button @click="deleteName">删除name属性</button>
+  <div class="app">
+    <h3>app</h3>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>
+      </template>
+      <template v-slot:fallback>
+        <h3>加载中...</h3>
+      </template>
+    </Suspense>
   </div>
+
 </template>
 
 <script>
-  import {reactive} from 'vue'
+  //静态引入
+  // import Child from './components/Child'
 
+  //异步引入
+  import {defineAsyncComponent} from 'vue'
+  const Child = defineAsyncComponent(()=>import('./components/Child')) 
   export default {
-    name: 'App',
-
-    //测试
-    setup(){
-      //数据
-      let person = reactive({
-        name:'王五',
-        age:18,
-        job:{
-          type:'前端工程师',
-          salary:'30k',
-          a:{
-            b:{
-              c:666
-            }
-          }
-        },
-        salary:'20k',
-        hobby:['抽烟','喝酒']
-      })
-
-      function changeInfo(){
-        person.name= 'lili'
-        person.age = 66
-        person.hobby[0] = '学习'
-
-        // console.log('更改',job);
-      }
-
-      function addSex(){
-        person.sex = '女'
-      }
-
-      function deleteName(){
-        delete person.name
-      }
-
-      //返回一个对象（常用
-      return{
-        person,
-        changeInfo,
-        addSex,
-        deleteName
-
-      }
-    }
+    name:'App',
+    components:{Child},
   }
-
 </script>
 
+<style>
+  .app{
+    background-color: gray;
+    padding: 10px;
+  }
+</style>
