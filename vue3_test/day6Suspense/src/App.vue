74659ca70cc03d62d7969092cf96e9dc21cddd28
金鱼 <2115756856@qq.com>
2023-10-28 19:12:48 +0800

    Create App.vue

---
+++ vue3_test/day6Suspense/src/App.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="app">
+    <h3>app</h3>
+    <Suspense>
+      <template v-slot:default>
+        <Child/>
+      </template>
+      <template v-slot:fallback>
+        <h3>加载中...</h3>
+      </template>
+    </Suspense>
+  </div>
+
+</template>
+
+<script>
+  //静态引入
+  // import Child from './components/Child'
+
+  //异步引入
+  import {defineAsyncComponent} from 'vue'
+  const Child = defineAsyncComponent(()=>import('./components/Child')) 
+  export default {
+    name:'App',
+    components:{Child},
+  }
+</script>
+
+<style>
+  .app{
+    background-color: gray;
+    padding: 10px;
+  }
+</style>
