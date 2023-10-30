f2a9fd4e9bfc74226b13b65ebcf005775521c3cd
金鱼 <2115756856@qq.com>
2023-10-30 23:33:24 +0800

    Create postcss.config.js

---
+++ TailwindCSS/postcss.config.js
@@ -0,0 +1,17 @@
+// postcss.config.js
+module.exports = {
+    plugins: {
+      'postcss-import': {},
+      'tailwindcss/nesting': 'postcss-nesting',
+      'postcss-preset-env': {
+        features: { 'nesting-rules': false },
+      },
+      tailwindcss: {},
+      autoprefixer: {},
+      ...(process.env.NODE_ENV === 'production' ? { cssnano: {} } : {})
+    },
+    plugins: [
+      require('tailwindcss'),
+      require('autoprefixer'),
+    ]
+  }
