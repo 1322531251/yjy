296ad9aa5ee98cb8b915e3320a81e8a593e0c7b1
金鱼 <2115756856@qq.com>
2023-10-31 21:15:50 +0800

    Create PrimaryButton.vue

---
+++ TailwindCSS/src/components/PrimaryButton.vue
@@ -0,0 +1,10 @@
+<template>
+    <button class="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
+      <slot/>
+    </button>
+</template>
+<script>
+    export default {
+        name:'PrimaryButton'
+    }
+</script>
