7b124c23b938de63e64e7e58c8ba891b029a1afa
金鱼 <2115756856@qq.com>
2023-11-02 20:27:46 +0800

    Update postcss.config.js

--- TailwindCSS/postcss.config.js
+++ TailwindCSS/postcss.config.js
@@ -1,7 +1,11 @@
 // postcss.config.js
 module.exports = {
     plugins: {
+
+      // 使用多个CSS文件
       'postcss-import': {},
+
+      
       'tailwindcss/nesting': 'postcss-nesting',
       'postcss-preset-env': {
         features: { 'nesting-rules': false },
